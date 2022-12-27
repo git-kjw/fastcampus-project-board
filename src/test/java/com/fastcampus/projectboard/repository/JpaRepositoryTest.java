4bb3c817a350f78099dfad8e59d7e6128dca523c
git-kjw <git.kjw@gmail.com>
2022-12-28 00:03:17 +0900

    #22 - 도메인 정보 수정
    userId를 유니크로 수정
    이에 따른 테스트케이스도 같이 수정

--- src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
+++ src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
@@ -51,7 +51,7 @@ class JpaRepositoryTest {
     void givenTestData_whenInserting_thenWorksFind(){
         //given
         long previousCount = articleRepository.count();
-        UserAccount userAccount = userAccountRepository.save(UserAccount.of("uno", "pw", null, null, null));
+        UserAccount userAccount = userAccountRepository.save(UserAccount.of("newUno", "pw", null, null, null));
         Article article = Article.of(userAccount, "new article", "new content", "#spring");
 
         //when
